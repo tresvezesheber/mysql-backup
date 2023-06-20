2ddcb510e8ee50abb9c89b16c770759a80b9f5d8
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2023-06-20 16:27:23 -0300

    Criacao Bean ModelMapper

--- src/main/java/br/com/hebio/mysqlbackup/common/ModelMapperConfig.java
+++ src/main/java/br/com/hebio/mysqlbackup/common/ModelMapperConfig.java
@@ -1,7 +1,16 @@
 package br.com.hebio.mysqlbackup.common;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        var modelMapper = new ModelMapper();
+
+
+        return modelMapper;
+    }
 }
